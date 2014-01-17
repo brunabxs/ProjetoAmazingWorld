86da20f97a9922b403b5c815b110db263c4889d3
Bruna Xavier <brunabxsantos@gmail.com>
2014-01-17 20:02:46 -0200

    Funções Util.*
    - Função Util.retrieveActionParams
    - Recupera informação do JSON dos parâmetros uma ação (tipo, nome e o
    elemento). Verifica se o parâmetro é do tipo esperado.
    - Função Util.__findActionParams__
    - Recupera informação dos parâmetros de uma ação (tipo e nome). Não faz
    verificação do tipo.
    - Função Util.__findActionParamsType__
    - Recupera o tipo dos parâmetros de uma ação

--- test/tests-util.js
+++ test/tests-util.js
@@ -16,3 +16,125 @@ test( "Util.find place by name", function() {
   var collection = [{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}];
   deepEqual(Util.find(collection, {name:'place3'}), {name:'place3', id:'id3'});
 });
+
+test( "Util.__findActionParamsTypes__ action with no paremeter", function() {
+  deepEqual(Util.__findActionParamsTypes__('show'), []);
+});
+
+test( "Util.__findActionParamsTypes__ action with one paremeter", function() {
+  deepEqual(Util.__findActionParamsTypes__('go to @places@'), ['places']);
+});
+
+test( "Util.__findActionParamsTypes__ action with more than one paremeter", function() {
+  deepEqual(Util.__findActionParamsTypes__('go to @places@ and exchange @objects@'), ['places', 'objects']);
+});
+
+test( "Util.__findActionParams__ same actions with no parameter", function() {
+  deepEqual(Util.__findActionParams__('show', 'show'), []);
+});
+
+test( "Util.__findActionParams__ same actions with one parameter", function() {
+  deepEqual(Util.__findActionParams__('show @places@', 'show place1'), [{type:'places', name:'place1'}]);
+});
+
+test( "Util.__findActionParams__ same actions with more than one parameter", function() {
+  deepEqual(Util.__findActionParams__('show @places@ and @objects@', 'show place1 and object1'), [{type:'places', name:'place1'}, {type:'objects', name:'object1'}]);
+});
+
+test( "Util.__findActionParams__ different actions", function() {
+  try {
+    Util.__findActionParams__('show', 'move');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match');
+  }
+});
+
+test( "Util.__findActionParams__ same actions with one parameter required and no parameter is passed", function() {
+  try {
+    Util.__findActionParams__('show @places@', 'show');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match');
+  }
+});
+
+test( "Util.__findActionParams__ same actions with no parameter required and one parameter is passed", function() {
+  try {
+    Util.__findActionParams__('show', 'show place1');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match');
+  }
+});
+
+test( "Util.__findActionParams__ same actions with two parameters required and one parameter is passed", function() {
+  try {
+    Util.__findActionParams__('show @places@ and @objects@', 'show place1');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match');
+  }
+});
+
+test( "Util.retrieveActionParams same actions with no parameter", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  deepEqual(Util.retrieveActionParams(json, 'show', 'show'), []);
+});
+
+test( "Util.retrieveActionParams same actions with one parameter same type", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  deepEqual(Util.retrieveActionParams(json, 'show @places@', 'show place1'), [{type:'places', name:'place1', item:{id:'id1', name:'place1'}}]);
+});
+
+test( "Util.retrieveActionParams same actions with one parameter different type", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  
+  try {
+    Util.retrieveActionParams(json, 'show @objects@', 'show place1');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match :: place1 does not belong to objects');
+  }
+});
+
+test( "Util.retrieveActionParams same actions with two parameters same type", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  deepEqual(Util.retrieveActionParams(json, 'show @places@ and @objects@', 'show place1 and obj2'), [{type:'places', name:'place1', item:{id:'id1', name:'place1'}}, {type:'objects', name:'obj2', item:{id:'idobj2', name:'obj2'}}]);
+});
+
+test( "Util.retrieveActionParams same actions with two parameter and second one with different type", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  
+  try {
+    Util.retrieveActionParams(json, 'show @objects@ and @places@', 'show obj2 and obj1');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match :: obj1 does not belong to places');
+  }
+});
+
+test( "Util.retrieveActionParams same actions with two parameter and both with different types", function() {
+  var json = {objects:[{id:'idobj1', name:'obj1'}, {id:'idobj2', name:'obj2'}], places:[{id:'id1', name:'place1'}, {id:'id2', name:'place2'}, {id:'id3', name:'place3'}]};
+  
+  try {
+    Util.retrieveActionParams(json, 'show @objects@ and @places@', 'show place3 and obj1');
+    ok(false, 'Expected exception');
+  }
+  catch(err) {
+    ok(true);
+    equal(err, 'Actions did not match :: place3 does not belong to objects');
+  }
+});
--- util.js
+++ util.js
@@ -1,5 +1,7 @@
 var Util = Util || {};
 
+Util.typeRegExp = new RegExp('@([^@]*)@', 'g');
+
 Util.find = function(collection, attributes) {
   if (collection === undefined || attributes === undefined) return;
  
@@ -16,4 +18,56 @@ Util.find = function(collection, attributes) {
 
     if (found) return collection[i];
   }
+};
+
+Util.retrieveActionParams = function(json, gameAction, playerAction) {
+    var actionParams = Util.__findActionParams__(gameAction, playerAction);
+    
+    for (var i = 0; i < actionParams.length; i++) {
+        var item = Util.find(json[actionParams[i].type], {name : actionParams[i].name});
+        if (item === undefined) {
+            throw 'Actions did not match :: ' + actionParams[i].name + ' does not belong to ' + actionParams[i].type;
+        }
+        
+        actionParams[i].item = item;
+    }
+    
+    return actionParams;
+};
+
+Util.__findActionParams__ = function(gameAction, playerAction) {
+    var gameAction = gameAction.replace(/[ ]*/g, '');
+    var playerAction = playerAction.replace(/[ ]*/g, '');
+    
+    var actions = [];
+    var types = Util.__findActionParamsTypes__(gameAction);
+    
+    if (types.length === 0 && gameAction !== playerAction)
+        throw 'Actions did not match';
+    
+    var regexp = new RegExp(gameAction.replace(Util.typeRegExp, '([_a-zA-z0-9]+)'), 'g');
+    
+    var match = regexp.exec(playerAction);
+    if (match !== null) {
+        for (var i = 0; i < types.length; i++) {
+            actions.push({type:types[i], name:match[i+1]});
+        }
+    }
+    else {
+        throw 'Actions did not match';
+    }
+    
+    return actions;
+};
+
+Util.__findActionParamsTypes__ = function(gameAction) {
+    var types = [];
+    
+    var match = Util.typeRegExp.exec(gameAction);
+    while (match !== null) {
+        types.push(match[1]);
+        match = Util.typeRegExp.exec(gameAction);
+    }
+    
+    return types;
 };
